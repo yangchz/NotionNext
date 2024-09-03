4d788b5e595ad372038a68e5fc374664f2147213
yangchz <914602509@qq.com>
2024-09-03 22:22:36 +0800

    修改：替换不蒜子显示为 umami。

--- blog.config.js
+++ blog.config.js
@@ -285,6 +285,10 @@ const BLOG = {
   WEB_WHIZ_BASE_URL:
     process.env.NEXT_PUBLIC_WEB_WHIZ_BASE_URL || 'https://api.webwhiz.ai', // 可以自建服务器
   WEB_WHIZ_CHAT_BOT_ID: process.env.NEXT_PUBLIC_WEB_WHIZ_CHAT_BOT_ID || null, // 在后台获取ID
+  UMAMI_ENABLED: process.env.NEXT_PUBLIC_UMAMI_ENABLED || false, // 是否开启
+  UMAMI_SCRIPT_URL: process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL || null,
+  UMAMI_WEBSITE_ID: process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID || null,
+  UMAMI_TOKEN: process.env.NEXT_PUBLIC_UMAMI_TOKEN || null,
   DIFY_CHATBOT_ENABLED: process.env.NEXT_PUBLIC_DIFY_CHATBOT_ENABLED || false,
   DIFY_CHATBOT_BASE_URL: process.env.NEXT_PUBLIC_DIFY_CHATBOT_BASE_URL || '',
   DIFY_CHATBOT_TOKEN: process.env.NEXT_PUBLIC_DIFY_CHATBOT_TOKEN || '',
